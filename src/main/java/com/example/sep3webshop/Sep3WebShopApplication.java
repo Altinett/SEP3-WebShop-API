fa1aadda861728311ecdf6c536356d5cbc2c68e7
AHowson <Alexander.Howson@gmail.com>
2023-10-20 11:40:55 +0200

    files

---
+++ src/main/java/com/example/sep3webshop/Sep3WebShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.sep3webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep3WebShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sep3WebShopApplication.class, args);
+    }
+
+}
